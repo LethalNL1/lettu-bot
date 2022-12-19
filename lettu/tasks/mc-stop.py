fe7e75ecb0ae281b217c5b2e84866707c6ca8021
LethalNL1 <jan.jacob.pebesma@gmail.com>
2022-12-19 22:22:53 +0100

    removed debug
    \

--- lettu/tasks/mc-stop.py
+++ lettu/tasks/mc-stop.py
@@ -16,32 +16,23 @@ async def mc_stop():
     try:
         server.ping()
         status = 0
-        print("Server is up")
         online = server.status().players.online
-        print(f"Online: {online}")
     except:
         status = 1
-        print("Server is down")
     if status == 0 and online == 0:
-        print("test")
         minutes = 0
         while online == 0:
             if minutes == 5:
-                print("Shutting down...")
                 os.system(f"ssh {credentials.USER}@{credentials.MC_IP} sudo shutdown now")
                 return
             else:
                 try:
                     server.ping()
                     online = server.status().players.online
-                    print(f"Online: {online}")
                     minutes += 1
                     time.sleep(60)
-                    print(f"Minutes: {minutes}")
                 except:
-                    print("Server unreachable, exiting...")
                     return
-        print("Server not empty, exiting...")
         return
 
 
